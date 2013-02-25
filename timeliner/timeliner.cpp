13e3b961b27e31c026c83c9579816ad40ea66f91
Camille Goudeseune <cog@illinois.edu>
2013-02-25 11:24:30 -0600

    visual studio 2010: ignore these files

---
+++ timeliner/timeliner.cpp
@@ -0,0 +1,13 @@
+// timeliner.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+extern int main(int argc, char* argv[]);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+// todo: use stackoverflow recipe to convert TCHAR to char, so this will actually run.  Sigh.
+	return main(0, NULL);
+}
+
